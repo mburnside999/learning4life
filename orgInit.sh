347f462077513f4222ddad88ff43869af21f4c12
Mike Burnside <mburnside+git@salesforce.com>
2022-04-05 18:02:00 +1000

    changes to support EE, by removing Data.com and more

--- orgInit.sh
+++ orgInit.sh
@@ -4,3 +4,6 @@ sfdx force:source:push
 sfdx force:user:permset:assign -n L4LPS
 sfdx shane:user:password:set -p sfdx1234 -g User -l User
 sfdx force:org:open -p "/lightning/o/Contact/list?filterName=AllContacts"
+//sfdx force:user:create --setalias qa-user --definitionfile config/user-def.json
+//sfdx shane:user:password:set -p sfdx1234 -g Mike -l DeHennin
+//sfdx force:apex:execute -f ./config/apex/load.apex
