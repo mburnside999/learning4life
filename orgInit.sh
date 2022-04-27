7639fab25a2206aef1f23954e52c42c53124d438
Mike Burnside <mburnside+git@salesforce.com>
2022-04-27 16:31:12 +1000

    more packaging

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
-sfdx force:org:delete -u L4Lscratch
-sfdx shane:org:create --verbose --userprefix=mike --userdomain=l4ldemo.com -f config/project-scratch-def.json -d 30 -s -a L4Lscratch 
+sfdx force:org:delete -u L4LscratchMOREJUNK
+sfdx shane:org:create --verbose --userprefix=mike --userdomain=l4ldemo.com -f config/project-scratch-def.json -d 30 -s -a L4LscratchMOREJUNK
 sfdx force:source:push
 sfdx force:user:permset:assign -n L4LPS
 sfdx shane:user:password:set -p sfdx1234 -g User -l User
