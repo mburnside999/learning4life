79952256abc36d7353f22d8febfc0c6d2ca82894
Mike Burnside <mburnside+git@salesforce.com>
2022-05-31 08:53:53 +1000

    nebula

--- orgInit.sh
+++ orgInit.sh
@@ -1,9 +1,9 @@
-sfdx force:org:delete -u L4Lscratch2
-sfdx shane:org:create --verbose --userprefix=mike --userdomain=l4ldemo.com -f config/project-scratch-def.json -d 30 -s -a L4Lscratch2
+sfdx force:org:delete -u L4Lscratch3
+sfdx shane:org:create --verbose --userprefix=mike --userdomain=l4ldemo.com -f config/project-scratch-def.json -d 30 -s -a L4Lscratch3
 
 # install nebula logging and assign permissions
 sfdx force:package:install -w 20 -r -p  04t5Y0000015lXSQAY
-sfdx force:user:permset:assign -n Logger:Admin
+sfdx force:user:permset:assign -n LoggerAdmin
 sfdx force:user:permset:assign -n LoggerEndUser	
 sfdx force:user:permset:assign -n LoggerLogCreator
 sfdx force:user:permset:assign -n LoggerLogViewer
@@ -15,4 +15,4 @@ sfdx force:org:open -p "/lightning/o/Contact/list?filterName=AllContacts"
 //sfdx force:user:create --setalias qa-user --definitionfile config/user-def.json
 //sfdx shane:user:password:set -p sfdx1234 -g Mike -l DeHennin
 //sfdx force:apex:execute -f ./config/apex/load.apex
-sfdx texei:data:import --inputdir ./sfdx-out --targetusername L4Lscratch2
+sfdx texei:data:import --inputdir ./sfdx-out --targetusername L4Lscratch3
