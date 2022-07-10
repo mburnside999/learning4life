ab151fde29640a0b61bdfdda6fcf877f2636fbd7
Mike Burnside <mburnside+git@salesforce.com>
2022-07-11 09:32:56 +1000

    Test

--- orgInit.sh
+++ orgInit.sh
@@ -12,7 +12,8 @@ sfdx force:source:push --loglevel=debug
 sfdx force:user:permset:assign -n L4LPS
 sfdx shane:user:password:set -p sfdx1234 -g User -l User
 sfdx force:org:open -p "/lightning/o/Contact/list?filterName=AllContacts"
-sfdx force:apex:test:run --testlevel RunLocalTests
+sfdx force:apex:test:run --classnames  "TestLFLInvocableSessionStatistics,TestL4LController,TestLFLClientObjectiveHelper,TestLFLInvocableReportWizard"  -r human --codecoverage 
+sfdx force:data:soql:query -q 'SELECT ApexClassOrTriggerId,ApexClassOrTrigger.Name,NumLinesCovered,NumLinesUncovered FROM ApexCodeCoverageAggregate ORDER BY ApexClassOrTrigger.Name' -t
 #sfdx force:user:create --setalias qa-user --definitionfile config/user-def.json
 #sfdx shane:user:password:set -p sfdx1234 -g Mike -l DeHennin
 #sfdx force:apex:execute -f ./config/apex/load.apex
