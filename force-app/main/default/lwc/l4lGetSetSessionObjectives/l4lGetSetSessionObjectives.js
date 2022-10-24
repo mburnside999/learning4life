ea9ec077595a23bcc6a9a11a0be13d7aed0bcb6e
Mike Burnside <mburnside+git@salesforce.com>
2022-10-24 17:00:11 +1100

    logging

--- force-app/main/default/lwc/l4lGetSetSessionObjectives/l4lGetSetSessionObjectives.js
+++ force-app/main/default/lwc/l4lGetSetSessionObjectives/l4lGetSetSessionObjectives.js
@@ -92,7 +92,7 @@ const columns = [
     type: "button-icon",
     initialWidth: 40,
     typeAttributes: {
-      iconName: { fieldName: "rowIcon" },
+      iconName: "utility:quick_text",
       name: "detail",
       variant: "border-filled",
       value: "detail"
@@ -158,9 +158,9 @@ const lockedcolumns = [
     type: "button-icon",
     initialWidth: 40,
     typeAttributes: {
-      iconName: { fieldName: "rowIcon" },
+      iconName: "utility:quick_text",
       name: "detail",
-      variant: "border-filled",
+      variant: "border_filled",
       value: "detail"
     }
   }
@@ -284,16 +284,8 @@ export default class L4lGetSetSessionObjectives extends LightningElement {
 
     getSessionObjectives({ sess: this.recordId })
       .then((result) => {
-        this.sessionObjectives = result.map((row) => ({
-          ...row,
-          rowIcon: row.Comment__c != null ? "utility:quick_text" : null
-        }));
-
-        console.log(`this.sessionObjectives=${this.sessionObjectives}`);
-        this.allObjectives = this.sessionObjectives;
-
-        //this.sessionObjectives=result;
-        //this.allObjectives = result;
+        this.sessionObjectives = result;
+        this.allObjectives = result;
 
         this.logit(
           DEBUG,
