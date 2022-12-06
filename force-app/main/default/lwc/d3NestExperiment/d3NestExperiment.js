d12973d7f7a56f2cabd7bcd1d97ef2b896fa8485
Mike Burnside <mburnside@salesforce.com>
2022-12-06 16:50:08 +1100

    colors

--- force-app/main/default/lwc/d3NestExperiment/d3NestExperiment.js
+++ force-app/main/default/lwc/d3NestExperiment/d3NestExperiment.js
@@ -86,6 +86,35 @@ export default class D3NestExperiment extends LightningElement {
 
     console.log("NEST in initializeD3()");
 
+    // const mycolor = (area) => {
+    //   console.log("area=" + area);
+    //   let colorarray = [
+    //     "#1f77b4",
+    //     "#ff7f0e",
+    //     "#2ca02c",
+    //     "#d62728",
+    //     "#9467bd",
+    //     "#8c564b",
+    //     "#e377c2",
+    //     "#7f7f7f",
+    //     "#bcbd22",
+    //     "#17becf"
+    //   ];
+
+    //   let hash = area
+    //     .split("")
+    //     .reduce(
+    //       (prevHash, currVal) =>
+    //         ((prevHash << 5) - prevHash + currVal.charCodeAt(0)) | 0,
+    //       0
+    //     );
+    //   console.log(Math.abs(hash));
+
+    //   hash = hash % 10;
+
+    //   return colorarray[hash];
+    // };
+
     var margin = { top: 10, right: 10, bottom: 10, left: 10 },
       width = 1400 - margin.left - margin.right,
       height = 800 - margin.top - margin.bottom;
@@ -136,9 +165,13 @@ export default class D3NestExperiment extends LightningElement {
         return d.y1 - d.y0;
       })
       .style("fill", function (d) {
+        //return mycolor(d.parent.data.name);
         return d.data.value == 1 ? "#EBF5F2" : "#72B9A8";
         //return d.data.value == 1 ? "#838996	" : "#6AB3A3";
         //return d.data.value == 1 ? "#E7F0F7" : "#91BE5A";
+      })
+      .style("opacity", function (d) {
+        return 0.5;
       })
       .style("stroke", "black");
 
