5b63056f8c588271624085424b21cefa93712cfe
mburnside <mburnside@salesforce.com>
2024-01-30 00:07:41 +1100

    tree

--- force-app/main/default/lwc/lflTreeView/lflTreeView.js
+++ force-app/main/default/lwc/lflTreeView/lflTreeView.js
@@ -1,22 +1,54 @@
 import { LightningElement, wire } from "lwc";
 import getJSONTree from "@salesforce/apex/LFLTreeUtil.getJSONTree";
 
+const COLUMNS = [
+  {
+    type: "text",
+    fieldName: "name",
+    label: "Name"
+  },
+  {
+    type: "text",
+    fieldName: "type",
+    label: "Type"
+  },
+  {
+    type: "text",
+    fieldName: "status",
+    label: "Status"
+  },
+  {
+    type: "url",
+    fieldName: "IdUrl",
+    label: "Go To Record Page",
+    typeAttributes: { label: { fieldName: "name" }, target: "_blank" }
+  }
+];
+
 import { logDebug, logInfo, logError } from "c/l4lNebulaUtil";
 export default class LflTreeView extends LightningElement {
-  treeview = [];
+  gridData = [];
+  gridColumns = COLUMNS;
   @wire(getJSONTree, { reserved: "x" })
   wiredJSON(value) {
-    console.log("yyyyyyyy");
     const { data, error } = value;
-    console.log("zzzzzzzzzz" + JSON.stringify(value));
     if (data) {
       console.log("xxxxxxxxx");
       console.log(data);
-      this.treeview = JSON.parse(data);
+      this.gridData = JSON.parse(data);
+      this.allData = this.gridData;
       this.error = undefined;
     } else if (error) {
       this.error = error;
-      this.treeview = undefined;
+      this.gridData = undefined;
     }
+  }
+
+  handleFilterKeyInput(event) {
+    const filterKey = event.target.value.toLowerCase();
+
+    this.gridData = this.allData.filter((so) =>
+      so.name.toLowerCase().includes(filterKey)
+    );
   }
 }
