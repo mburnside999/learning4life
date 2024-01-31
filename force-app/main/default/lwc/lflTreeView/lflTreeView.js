a2762c3dc5108379e208eb5bbee2d7c7d51812d6
mburnside <mburnside@salesforce.com>
2024-02-01 07:49:48 +1100

    catalog

--- force-app/main/default/lwc/lflTreeView/lflTreeView.js
+++ force-app/main/default/lwc/lflTreeView/lflTreeView.js
@@ -28,9 +28,10 @@ const COLUMNS = [
 import { logDebug, logInfo, logError } from "c/l4lNebulaUtil";
 export default class LflTreeView extends LightningElement {
   gridData = [];
+  o = 0;
   gridColumns = COLUMNS;
   @api lwcTitle = "Catalog Browser";
-
+  @api isLoaded = false;
   @wire(getJSONTree, { reserved: "x" })
   wiredJSON(value) {
     const { data, error } = value;
@@ -39,17 +40,75 @@ export default class LflTreeView extends LightningElement {
       this.gridData = JSON.parse(data);
       this.allData = this.gridData;
       this.error = undefined;
+      this.isLoaded = true;
     } else if (error) {
       this.error = error;
       this.gridData = undefined;
     }
   }
 
+  get resultSize() {
+    return this.gridData.length;
+  }
+
+  get catalogShape() {
+    let y = [];
+    let z = [];
+    let ptotal = this.gridData.length;
+    let sdtotal = 0;
+    let objtotal = 0;
+
+    for (let i = 0; i < ptotal; i++) {
+      y = this.gridData[i]._children;
+      sdtotal += y.length;
+      for (let j = 0; j < y.length; j++) {
+        z = y[j]._children;
+        objtotal += z.length;
+      }
+    }
+    console.log("ptotal=" + ptotal);
+    console.log("sdtotal=" + sdtotal);
+    console.log("objtotal=" + objtotal);
+
+    return (
+      " Programs: " + ptotal + ", SDs: " + sdtotal + ", Objectives: " + objtotal
+    );
+  }
+
+  get countA() {
+    let total = 0;
+    this.gridData.forEach((entry) => (total += entry._children.length));
+    // this.gridData._children._children.forEach(
+    //   (entry) => (grandtotal += entry._children.length)
+    // );
+    return total;
+  }
+
+  get countB() {
+    let x = [];
+    let y = [];
+    let grandtotal = 0;
+
+    for (let i = 0; i < this.gridData.length; i++) {
+      x = this.gridData[i]._children;
+      console.log("zzzzzz" + x.length);
+      for (let j = 0; j < x.length; j++) {
+        y = x[j]._children;
+        grandtotal += y.length;
+      }
+      return grandtotal;
+    }
+  }
+
   handleFilterKeyInput(event) {
     const filterKey = event.target.value.toLowerCase();
+    if (filterKey.length == 0) {
+      this.gridData = this.allData;
+      console.log("REFRESH");
+    }
 
-    this.gridData = this.allData.filter((so) =>
-      so.name.toLowerCase().includes(filterKey)
-    );
+    this.gridData = this.allData.filter((so) => {
+      return so.name.toLowerCase().includes(filterKey);
+    });
   }
 }
